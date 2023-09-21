e2dba8e60c2b25d78ea21e1d1d5c2def87aafae6
Jayanthbabu456 <babujayanth456@gmail.com>
2023-09-21 20:09:30 +0530

    Palindrome

---
+++ basicprograms/palindrome/Palindrome.java
@@ -0,0 +1,8 @@
+// To find out whether the given String is Palindrome or not.
+package basicprograms.palindrome;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        System.out.println("Palindrome");
+    }
+}
