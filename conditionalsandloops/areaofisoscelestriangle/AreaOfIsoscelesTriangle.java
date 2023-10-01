a873b0ea50b7da6ea56f1aa2ff67a26afeac2d22
Jayanthbabu456 <babujayanth456@gmail.com>
2023-10-01 21:18:02 +0530

    IsoscelesTriangle

---
+++ conditionalsandloops/areaofisoscelestriangle/AreaOfIsoscelesTriangle.java
@@ -0,0 +1,7 @@
+package conditionalsandloops.areaofisoscelestriangle;
+
+public class AreaOfIsoscelesTriangle {
+    public static void main(String[] args) {
+        System.out.println("Area Of Isosceles Triangle");
+    }
+}
