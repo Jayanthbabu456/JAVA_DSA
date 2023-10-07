5521d1da9acc1ebb6fe59bac26d33e5816664f8b
Jayanthbabu456 <babujayanth456@gmail.com>
2023-10-07 21:10:42 +0530

    factorial

---
+++ conditionalsandloops/factorial/Factorial.java
@@ -0,0 +1,7 @@
+package conditionalsandloops.factorial;
+
+public class Factorial {
+    public static void main(String[] args) {
+        System.out.println("Factorial");
+    }
+}
---
+++ conditionalsandloops/largestuntilzero/Largestuntilzero.java
@@ -0,0 +1,10 @@
+// Take integer inputs till the user enters 0 and print the largest number from all.
+package conditionalsandloops.largestuntilzero;
+
+import java.util.Scanner;
+
+public class Largestuntilzero {
+    public static void main(String[] args) {
+       
+    }
+}
