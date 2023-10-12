491870d17141dc4eaff67a1c75c4448f05ae25a4
Jayanthbabu456 <babujayanth456@gmail.com>
2023-10-12 20:54:03 +0530

    perfect number

---
+++ conditionalsandloops/perfectnumber/PerfectNumber.java
@@ -0,0 +1,8 @@
+// Perfect Number In Java
+package conditionalsandloops.perfectnumber;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        System.out.println("Perfect Number");
+    }
+}
