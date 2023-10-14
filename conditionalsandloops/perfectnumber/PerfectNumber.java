0fcf25fd852fdf1c429e774fd623d21e562d3c02
Jayanthbabu456 <babujayanth456@gmail.com>
2023-10-14 11:32:48 +0530

    perfect number

--- conditionalsandloops/perfectnumber/PerfectNumber.java
+++ conditionalsandloops/perfectnumber/PerfectNumber.java
@@ -1,8 +1,25 @@
 // Perfect Number In Java
 package conditionalsandloops.perfectnumber;
 
+import java.util.Scanner;
+
 public class PerfectNumber {
     public static void main(String[] args) {
-        System.out.println("Perfect Number");
+        Scanner input=new Scanner(System.in);
+        int num=input.nextInt();
+        int sum=0;
+        for (int i = 1; i <= num/2; i++) {
+             if(num%i==0){
+                System.out.print(i +" ");
+               sum=sum+i; 
+             }
+        }
+        if(sum==num){
+            System.out.println("Perfect Number");
+        }
+       else{
+        System.out.println("Not Perfect Number");
+       }
+        input.close();
     }
 }
