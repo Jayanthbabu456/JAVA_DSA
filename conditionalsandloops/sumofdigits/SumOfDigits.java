f9dd37e9448af97f4656d93bde9a8fd4894ec956
Jayanthbabu456 <babujayanth456@gmail.com>
2023-10-16 10:01:55 +0530

    sum of digits

--- conditionalsandloops/sumofdigits/SumOfDigits.java
+++ conditionalsandloops/sumofdigits/SumOfDigits.java
@@ -1,7 +1,20 @@
+// Sum Of A Digits Of Number
 package conditionalsandloops.sumofdigits;
+
+import java.util.Scanner;
 
 public class SumOfDigits {
     public static void main(String[] args) {
-        System.out.println("Sum of digits");
+        Scanner input=new Scanner(System.in);
+        int num=input.nextInt();
+        int sum=0;
+        while(num!=0){
+           int rem=num%10;
+           sum=sum+rem;
+           num=num/10;
+
+        }
+        System.out.println(sum);
+        input.close();
     }
 }
