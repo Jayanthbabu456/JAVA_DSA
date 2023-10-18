66176e7a1c8c659b8f2d8721cf03785108189e7a
Jayanthbabu456 <babujayanth456@gmail.com>
2023-10-18 10:06:27 +0530

    evenodd in functions

---
+++ functions/evenodd/EvenOdd.java
@@ -0,0 +1,24 @@
+package functions.evenodd;
+
+import java.util.Scanner;
+
+/**
+ * EvenOdd
+ */
+public class EvenOdd {
+    static void evenodd(int num){
+        if(num%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int n=input.nextInt();
+        evenodd(n);
+        input.close();
+    }
+}
