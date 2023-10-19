b04164943362ef7e4e7d36f93c776c971cf77290
Jayanthbabu456 <babujayanth456@gmail.com>
2023-10-19 20:54:00 +0530

    prime

---
+++ functions/prime/Prime.java
@@ -0,0 +1,8 @@
+// Define a method to find out if a number is prime or not.
+package functions.prime;
+
+public class Prime {
+    public static void main(String[] args) {
+        System.out.println("Prime or not");
+    }
+}
