5b5a61329b1bb23908b5c3e95b19105fd52bac25
Jayanthbabu456 <babujayanth456@gmail.com>
2023-10-24 20:32:33 +0530

    Pythagorean triplet

---
+++ functions/pythagoreantriplet/Pythagoreantriplet.java
@@ -0,0 +1,11 @@
+package functions.pythagoreantriplet;
+
+/**
+ * Pythagoreantriplet
+ */
+public class Pythagoreantriplet {
+
+    public static void main(String[] args) {
+        System.out.println("Pythagorean triplet");
+    }
+}
