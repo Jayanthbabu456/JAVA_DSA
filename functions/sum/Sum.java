e88382cc5737ebc13a7389e87e60e7c9fb7c8af7
Jayanthbabu456 <babujayanth456@gmail.com>
2023-10-28 20:50:07 +0530

    sum

--- functions/sum/Sum.java
+++ functions/sum/Sum.java
@@ -1,7 +1,17 @@
 package functions.sum;
 
+import java.util.Scanner;
+
 public class Sum {
+    static void Calculatesum(int a,int b){
+        int c=a+b;
+        System.out.println(c);
+    }
     public static void main(String[] args) {
-        System.out.println("Sum using function");
+        Scanner input=new Scanner(System.in);
+        int num1=input.nextInt();
+        int num2=input.nextInt();
+        Calculatesum(num1, num2);
+        input.close();
     }
 }
