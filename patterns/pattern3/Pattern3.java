124e4702d26d9beb0f384b95b8b6f8029125046f
Jayanthbabu456 <babujayanth456@gmail.com>
2023-11-16 15:58:24 +0530

    pattern3

--- patterns/pattern3/Pattern3.java
+++ patterns/pattern3/Pattern3.java
@@ -1,6 +1,24 @@
+// *****
+// ****
+// ***
+// **
+// *
+
 package patterns.pattern3;
+
+import java.util.Scanner;
+
 public class Pattern3 {
     public static void main(String[] args) {
-        System.out.println("Pattern3");
+        Scanner in =new Scanner(System.in);
+        int num=in.nextInt();
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j <num-i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        in.close();
     }
+
 }
