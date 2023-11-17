53ee7e7a78daa091c32659aa3b21c274880e1a60
Jayanthbabu456 <babujayanth456@gmail.com>
2023-11-17 16:04:47 +0530

    pattern4

--- patterns/pattern4/Pattern4.java
+++ patterns/pattern4/Pattern4.java
@@ -1,7 +1,17 @@
 package patterns.pattern4;
 
+import java.util.Scanner;
+
 public class Pattern4 {
     public static void main(String[] args) {
-        System.out.println("Patter4");
+        Scanner in=new Scanner(System.in);
+        int  num=in.nextInt();
+        for (int i = 1; i <=num; i++) {
+            for (int j = 1; j <=i; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+        in.close();
     }
 }
