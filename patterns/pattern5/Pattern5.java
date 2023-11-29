b296433f174af38a212fe3869f341365dc08d587
Jayanthbabu456 <babujayanth456@gmail.com>
2023-11-29 10:13:02 +0530

    pattern5

--- patterns/pattern5/Pattern5.java
+++ patterns/pattern5/Pattern5.java
@@ -1,7 +1,28 @@
+    // *
+    // **
+    // ***
+    // ****
+    // *****
+    // ****
+    // ***
+    // **
+    // *
 package patterns.pattern5;
+
+import java.util.Scanner;
 
 public class Pattern5 {
     public static void main(String[] args) {
-        System.out.println("Pattern5");
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        for (int row = 0; row <2*n; row++) {
+            int totalcols=row>n ?2*n-row : row;
+            for (int col = 0; col < totalcols; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        in.close();
+
     }
 }
