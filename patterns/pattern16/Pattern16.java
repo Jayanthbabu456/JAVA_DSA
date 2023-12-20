7f36c4fc9656fa9781e969a7e3ec0ada6dc09db4
Jayanthbabu456 <babujayanth456@gmail.com>
2023-12-20 19:53:25 +0530

    pattern16

--- patterns/pattern16/Pattern16.java
+++ patterns/pattern16/Pattern16.java
@@ -1,3 +1,8 @@
+//         1
+//       1   1
+//     1   2   1
+//   1   3   3   1
+// 1   4   6   4   1
 public class Pattern16 {
     public static void main(String[] args) {
         System.out.println("Pattern16");
