6abcd0e2e073f85c7e152e2b575cfaf7cf89b39f
Jayanthbabu456 <babujayanth456@gmail.com>
2024-01-03 20:46:47 +0530

    Pattern30

--- patterns/pattern30/Pattern30.java
+++ patterns/pattern30/Pattern30.java
@@ -1,3 +1,10 @@
+        //  4 4 4 4 4 4 4  
+        //  4 3 3 3 3 3 4   
+        //  4 3 2 2 2 3 4   
+        //  4 3 2 1 2 3 4   
+        //  4 3 2 2 2 3 4   
+        //  4 3 3 3 3 3 4   
+        //  4 4 4 4 4 4 4   
 public class Pattern30 {
     public static void main(String[] args) {
         System.out.println("Pattern30");
