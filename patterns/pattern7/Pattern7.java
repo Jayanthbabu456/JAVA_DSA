4cdec3f6d95501b4c3dc6ffd0f57edab4736747f
Jayanthbabu456 <babujayanth456@gmail.com>
2024-01-05 20:08:46 +0530

    pattern7

--- patterns/pattern7/Pattern7.java
+++ patterns/pattern7/Pattern7.java
@@ -5,8 +5,25 @@
     //      *
 package patterns.pattern7;
 
+
+import java.util.Scanner;
+
 public class Pattern7 {
     public static void main(String[] args) {
-        System.out.println("Pattern7");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
     }
 }
