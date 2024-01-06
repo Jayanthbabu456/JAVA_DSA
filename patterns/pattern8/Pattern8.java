ac9f0c9eb5d57170d7f71014dce59186d5f114f8
Jayanthbabu456 <babujayanth456@gmail.com>
2024-01-06 20:31:39 +0530

    Pattern8

--- patterns/pattern8/Pattern8.java
+++ patterns/pattern8/Pattern8.java
@@ -5,9 +5,27 @@
     // *********
 package patterns.pattern8;
 
+import java.util.Scanner;
+
 public class Pattern8 {
 
     public static void main(String[] args) {
-        System.out.println("Pattern8");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int spaces = n - 1; 
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < spaces; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 0; k <= i * 2; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+            spaces--; 
+        }
     }
 }
