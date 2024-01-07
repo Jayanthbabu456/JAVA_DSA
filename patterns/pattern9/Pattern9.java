a10648002d2819afd8ce6cc96d8e216a9fb7b500
Jayanthbabu456 <babujayanth456@gmail.com>
2024-01-07 21:47:18 +0530

    Pattern9

--- patterns/pattern9/Pattern9.java
+++ patterns/pattern9/Pattern9.java
@@ -5,8 +5,21 @@
     //     *
 package patterns.pattern9;
 
+import java.util.Scanner;
+
 public class Pattern9 {
     public static void main(String[] args) {
-        System.out.println("Pattern9");
+        Scanner sc =new Scanner(System.in);
+            int n=sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < i; j++) {
+                    System.out.print(" ");
+                }
+                
+                for (int j = 0; j < (2*n)-(2*i+1); j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
     }
 }
