59a0d9a5c0054fe1eb2dac4ead1e3e26cd76739c
Jayanthbabu456 <babujayanth456@gmail.com>
2024-01-08 19:50:30 +0530

    pattern-10,11

--- patterns/pattern10/Pattern10.java
+++ patterns/pattern10/Pattern10.java
@@ -5,8 +5,21 @@
     //  * * * * *
 package patterns.pattern10;
 
+import java.util.Scanner;
+
 public class Pattern10 {
     public static void main(String[] args) {
-        System.out.println("Pattern10");
+        Scanner sc =new Scanner(System.in);
+        int n=sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                System.out.print(" ");
+            }
+                for (int j = 0; j <=i; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+            sc.close();
     }
 }
--- patterns/pattern11/Pattern11.java
+++ patterns/pattern11/Pattern11.java
@@ -5,9 +5,21 @@
     //      *
 package patterns.pattern11;
 
+import java.util.Scanner;
+
 public class Pattern11 {
 
     public static void main(String[] args) {
-        System.out.println("Pattern11");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < n-i-1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
