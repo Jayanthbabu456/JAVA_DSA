2d133790aadbd6090cc8227c7359c01182e5a91c
Jayanthbabu456 <babujayanth456@gmail.com>
2024-01-12 21:13:59 +0530

    Pattern12

--- patterns/pattern12/Pattern12.java
+++ patterns/pattern12/Pattern12.java
@@ -10,8 +10,29 @@
 // * * * * *
 package patterns.pattern12;
 
+import java.util.Scanner;
+
 public class Pattern12 {
     public static void main(String[] args) {
-        System.out.println("Pattern12");
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= n-i-1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 }
