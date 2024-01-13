cfd7742a2fc26c630bc2dfbb6bcc3766e59635f4
Jayanthbabu456 <babujayanth456@gmail.com>
2024-01-13 20:56:14 +0530

    pattern13

--- patterns/pattern13/Pattern13.java
+++ patterns/pattern13/Pattern13.java
@@ -5,8 +5,27 @@
 //  *********
 package patterns.pattern13;
 
+import java.util.Scanner;
+
 public class Pattern13 {
     public static void main(String[] args) {
-        System.out.println("Pattern13");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2*i+1; k++) {
+                if(k==0 || k==2*i || i==4 )
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
