c2a1642c5db79ca854ea20e9ea831a8b3eb1442d
Jayanthbabu456 <babujayanth456@gmail.com>
2024-01-14 21:39:41 +0530

    pattern14

--- patterns/pattern14/Pattern14.java
+++ patterns/pattern14/Pattern14.java
@@ -5,8 +5,24 @@
 //      *
 package patterns.pattern14;
 
+import java.util.Scanner;
+
 public class Pattern14 {
     public static void main(String[] args) {
-        System.out.println("Pattern14");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * n - 1 - 2 * i; j++) {
+                if (j == 0 || j == 2 * n - 2 * i - 2 || i==0) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
