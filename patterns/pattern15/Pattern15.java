62c6efdc62d49c760f72a8f5c4e4f82f6a3c2114
Jayanthbabu456 <babujayanth456@gmail.com>
2024-01-15 20:53:05 +0530

    pattern15

--- patterns/pattern15/Pattern15.java
+++ patterns/pattern15/Pattern15.java
@@ -9,8 +9,39 @@
     //      *
 package patterns.pattern15;
 
+import java.util.Scanner;
+
 public class Pattern15 {
     public static void main(String[] args) {
-        System.out.println("Pattern15");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                if (j == 0 || j == 2 * i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * (n - i - 1) - 1; j++) {
+                if (j == 0 || j == 2 * (n - i - 1) - 2) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
