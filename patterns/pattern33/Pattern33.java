ed3a98d76666d5ad222488679b00db8e7e6d24fe
Jayanthbabu456 <babujayanth456@gmail.com>
2024-01-25 20:32:49 +0530

    pattern33

---
+++ patterns/pattern33/Pattern33.java
@@ -0,0 +1,11 @@
+    //    1      1
+    //    12    21
+    //    123  321
+    //    12344321
+package patterns.pattern33;
+
+public class Pattern33 {
+    public static void main(String[] args) {
+        System.out.println("Pattern33");
+    }
+}
