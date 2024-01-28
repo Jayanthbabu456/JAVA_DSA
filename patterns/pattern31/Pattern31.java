369adbd64ac498b02c6fec584c343f25e771faf1
Jayanthbabu456 <babujayanth456@gmail.com>
2024-01-28 21:41:03 +0530

    Pattern31

--- patterns/pattern31/Pattern31.java
+++ patterns/pattern31/Pattern31.java
@@ -6,8 +6,21 @@
 
 package patterns.pattern31;
 
+import java.util.Scanner;
+
 public class Pattern31 {
     public static void main(String[] args) {
-        System.out.println("Pattern31");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print((char) ('A' - 1 + j));
+                if (j > 1) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
