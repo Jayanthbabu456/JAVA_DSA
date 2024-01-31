a5913400e126f9181d5a938a53cdcbca1389f366
Jayanthbabu456 <babujayanth456@gmail.com>
2024-01-31 21:18:22 +0530

    Pattern32

--- patterns/pattern32/Pattern32.java
+++ patterns/pattern32/Pattern32.java
@@ -5,8 +5,19 @@
 // A B C D E
 package patterns.pattern32;
 
+import java.util.Scanner;
+
 public class Pattern32 {
     public static void main(String[] args) {
-        System.out.println("Pattern32");
+       Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            
+            for (char ch = (char) ('A' + n - i); ch <= 'E'; ch++) {
+                System.out.print(ch + " ");
+            }
+
+            System.out.println();
+        }
     }
 }
